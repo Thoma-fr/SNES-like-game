cbe5d8b8af4f329e6b751fc93ed5697653cb7535
Furyn <vincent_1999@hotmail.fr>
2022-09-08 10:29:10 +0200

    [ADD] Add light to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Info")]
     public Rigidbody2D _rb;
-    public float _speed;
+    public float _actual_speed;
+
+    [Header("Set Movement")]
+    public float _speed_jump = 20;
     public float _force = 100;
     public float _maxSpeed = 20;
     private bool _action = false;
     private Vector3 target = Vector3.zero;
     private float step;
 
+    [Header("Vignette")]
+    public Volume volume;
+    public float _vignette_speed = 0.001f;
+    public float _vignette_max = 0.5f;
+    private Vignette vignette;
+    private bool _light = false;
+
+    private void Start()
+    {
+
+        if (volume.profile.TryGet<Vignette>(out vignette))
+        {
+            vignette.intensity.overrideState = true;
+        }
+    }
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.LeftArrow) && !_action)
         {
             _action = true;
@@ -33,17 +56,43 @@ public class PlayerMovement : MonoBehaviour
             _rb.velocity = new Vector2(_maxSpeed, 0);
         }
 
-        _speed = _rb.velocity.x;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _light = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            _light = false;
+        }
+
+        if (_light)
+        {
+            Debug.Log("LIGHT ON");
+            vignette.intensity.value -= _vignette_speed / 1000;
+        }else if (vignette.intensity.value < _vignette_max)
+        {
+            Debug.Log("LIGHT OFF");
+            vignette.intensity.value += _vignette_speed / 1000;
+            if (vignette.intensity.value > _vignette_max)
+            {
+                vignette.intensity.value = _vignette_max;
+            }
+        }
+
+        _actual_speed = _rb.velocity.x;
 
         if (target != Vector3.zero)
         {
-            step = _speed * Time.deltaTime;
+            step = _speed_jump * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target, step);
             if (Vector3.Distance(transform.position,target) <= 1)
             {
+                _rb.velocity = new Vector2(_speed_jump, 0);
                 target = Vector3.zero;
             }
         }
+
+        
     }
 
     public void GoTo(Vector3 position)
